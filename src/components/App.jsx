4601f0294e03a2749d7ebe57099ffc64e354a639
Oleksandr FIlippov <filya170895@gmail.com>
2023-02-14 19:52:26 +0100

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import data from './data';
 import friends from './friends';
 import transactions from './transactions';
 import { Profile } from './profile';
-import { Statistics } from './statistics';
-import { FriendList } from './friendList';
-import { TransactionHistory } from './transactionHistory';
+import { Statistics } from './stats';
+import { FriendList } from './friendsData';
+import { TransactionHistory } from './history';
 
 export const App = () => {
   return (
