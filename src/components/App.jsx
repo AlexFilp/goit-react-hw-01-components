38957398a37a9773039320365e6badc15f0dcf16
Oleksandr FIlippov <filya170895@gmail.com>
2023-02-14 15:30:50 +0100

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import user from './user';
+import data from './data';
+import friends from './friends';
+import transactions from './transactions';
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
-      React homework
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <Statistics stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,20 @@
+export const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => {
+        return (
+          <li class="item" key={friend.id}>
+            <span class="status">{friend.isOnline}</span>
+            <img
+              class="avatar"
+              src={friend.avatar}
+              alt="User avatar"
+              width="48"
+            />
+            <p class="name">{friend.name}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
